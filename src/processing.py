d0d0180bb552c58880564a3e7e325d8f4a76df78
Paul Schatt <pschatt789@gmail.com>
2025-02-13 18:40:38 -0800

    Remove unecessary files

--- src/processing.py
+++ src/processing.py
@@ -1,1 +1,16 @@
 import os
+
+rootdir = "../realsense_overhead"
+
+
+#Remove unecessary files
+for subdir, dirs, files in os.walk(rootdir):
+    for file in files:
+        filename = os.path.join(subdir, file)
+        if "depth" in filename:
+            os.remove(filename)
+
+for subdir, dirs, files in os.walk(rootdir):
+    for file in files:
+        filename = os.path.join(subdir, file)
+        print(filename)
